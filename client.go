a3530f6873ce113728974b95f2d03f3c1953ac78
Mac21 <russ.tolle@gmail.com>
2020-06-12 14:30:00 -0600

    Make connection a pointer

--- client.go
+++ client.go
@@ -1,11 +1,16 @@
 package gomailman
 
 type Client struct {
-	conn Connection
+	conn *Connection
 }
 
-func NewClient(host, username, password string) *Client {
-	return &Client{
-		conn: NewConnection(host, username, password),
+func NewClient(host, username, password string) (*Client, error) {
+	conn, err := NewConnection(host, username, password)
+	if err != nil {
+		return nil, err
 	}
+
+	return &Client{
+		conn: conn,
+	}, nil
 }
