febf646bf337c127e4b71a92e4308ef1d18579a0
Mac21 <russ.tolle@gmail.com>
2020-06-15 11:12:41 -0600

    Add RequestError type to serialize errors from mailman

---
+++ error.go
@@ -0,0 +1,17 @@
+package gomailman
+
+import (
+	"fmt"
+)
+
+type RequestError struct {
+	error
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+var _ error = RequestError{}
+
+func (re RequestError) Error() string {
+	return fmt.Sprintf("%#v", re)
+}
