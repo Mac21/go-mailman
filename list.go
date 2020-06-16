c219b97339ad6e82b7dcaee44a4bc6613a17052b
Mac21 <russ.tolle@gmail.com>
2020-06-16 15:44:14 -0600

    Add List struct and client functions to get, add, delete

--- list.go
+++ list.go
@@ -1,23 +1,26 @@
 package gomailman
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-const (
-	ErrorListGet = "Error getting list"
+var (
+	// ErrorListGet is an error returned by GetList when response status != 2XX
+	ErrorListGet = errors.New("go-mailman: Error getting list")
+	// ErrorListAdd is an error returned by AddList when response status != 2XX
+	ErrorListAdd = errors.New("go-mailman: Error adding list")
+	// ErrorListDelete is an error returned by DeleteList when response status != 2XX
+	ErrorListAdd = errors.New("go-mailman: Error deleting list")
 )
 
 type List struct {
 	Name string `json:"list_name"`
 	ID   string `json:"list_id"`
-}
-
-func (l List) String() string {
-	return fmt.Sprintf("%#v", l)
 }
 
 func (c *Client) GetList(listID string) (*List, error) {
@@ -26,13 +29,18 @@ func (c *Client) GetList(listID string) (*List, error) {
 		return nil, err
 	}
 
-	if res.StatusCode/100 != 2 {
-		return nil, fmt.Errorf("%s: %s", ErrorListGet, res.Status)
-	}
-
 	b, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
+	}
+
+	if res.StatusCode/100 != 2 {
+		re := &RequestError{}
+		if err := json.Unmarshal(b, re); err != nil {
+			return nil, err
+		}
+
+		return nil, fmt.Errorf("%s: %s", ErrorListGet, re)
 	}
 
 	list := new(List)
@@ -42,4 +50,58 @@ func (c *Client) GetList(listID string) (*List, error) {
 	}
 
 	return list, res.Body.Close()
+}
+
+func (c *Client) AddList(list *List) error {
+	if list == nil {
+		return errors.New("go-mailman: Error adding nil list")
+	}
+
+	b, err := json.Marshal(list)
+	if err != nil {
+		return err
+	}
+
+	res, err := c.conn.do(http.MethodPost, c.buildURL("lists"), bytes.NewReader(b))
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode/100 != 2 {
+		b, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+
+		re := &RequestError{}
+		if err := json.Unmarshal(b, re); err != nil {
+			return err
+		}
+		return fmt.Errorf("%s %s", ErrorListAdd, re)
+	}
+
+	return res.Body.Close()
+}
+
+func (c *Client) DeleteList(listID string) error {
+	res, err := c.conn.do(http.MethodDelete, c.buildURL("lists", listID), http.NoBody)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode/100 != 2 {
+		b, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+
+		re := &RequestError{}
+		if err := json.Unmarshal(b, re); err != nil {
+			return err
+		}
+
+		return fmt.Errorf("%s %s", ErrorListDelete, re)
+	}
+
+	return res.Body.Close()
 }
