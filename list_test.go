78be0663a6e0768a0b6445ef33b3400f99df598a
Mac21 <russ.tolle@gmail.com>
2020-06-18 15:36:42 -0600

    Add tests for getting and deleting a list

--- list_test.go
+++ list_test.go
@@ -1,6 +1,7 @@
 package gomailman
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -27,7 +28,41 @@ func TestAddList(t *testing.T) {
 	if err := c.AddList(listID); err != nil {
 		t.Error(err)
 	}
+
 }
 
 func TestGetList(t *testing.T) {
+	c, err := NewClient(baseURL, username, password)
+	if err != nil {
+		t.Error(err)
+	}
+
+	list, err := c.GetList(listID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if list.MailHost != domainID {
+		t.Errorf("Unexpected list MailHost got %s expected %s", list.MailHost, domainID)
+	}
+
+	listsID := strings.Replace(listID, "@", ".", -1)
+	if list.ID != listsID {
+		t.Errorf("Unexpected list ID got %s expected %s", list.ID, listsID)
+	}
+}
+
+func TestDeleteList(t *testing.T) {
+	c, err := NewClient(baseURL, username, password)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := c.DeleteList(listID); err != nil {
+		t.Error(err)
+	}
+
+	t.Cleanup(func() {
+		c.DeleteDomain(domainID)
+	})
 }
