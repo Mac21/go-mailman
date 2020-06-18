1c28ff225d820da1243a53d5bb8bb5b07ff4a94f
Mac21 <russ.tolle@gmail.com>
2020-06-18 12:17:05 -0600

    Add TestAddList

--- list_test.go
+++ list_test.go
@@ -4,5 +4,30 @@ import (
 	"testing"
 )
 
+const (
+	listID          = "something@localhost.com"
+	listDescription = "A list about something"
+)
+
+func TestAddList(t *testing.T) {
+	c, err := NewClient(baseURL, username, password)
+	if err != nil {
+		t.Error(err)
+	}
+
+	domain := &Domain{
+		MailHost:    domainID,
+		Description: domainDescription,
+	}
+
+	if err := c.AddDomain(domain); err != nil {
+		t.Error(err)
+	}
+
+	if err := c.AddList(listID); err != nil {
+		t.Error(err)
+	}
+}
+
 func TestGetList(t *testing.T) {
 }
