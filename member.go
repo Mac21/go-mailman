c9020ab3213bca027e6d9e103c7d6242c7144738
Mac21 <russ.tolle@gmail.com>
2020-06-19 13:35:28 -0600

    Add AddListMember and GetListMembers and tests

--- member.go
+++ member.go
@@ -1,15 +1,79 @@
 package gomailman
 
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
 type Member struct {
 	ID               string           `json:"member_id"`
 	ListID           string           `json:"list_id"`
 	DisplayName      string           `json:"display_name"`
+	Email            string           `json:"email"`
 	DeliveryMode     DeliveryMode     `json:"delivery_mode"`
 	SubscriptionMode SubscriptionMode `json:"subscription_mode"`
 	ModerationAction Action           `json:"moderation_action"`
 	Role             MemberRole       `json:"role"`
 }
 
-func (c *Client) AddListMember(member *Member) error {
-	return nil
+type ListSubscriber struct {
+	ListID       string       `json:"list_id"`
+	Subscriber   string       `json:"subscriber"`
+	DisplayName  string       `json:"display_name"`
+	DeliveryMode DeliveryMode `json:"delivery_mode,omitempty"`
+	Role         MemberRole   `json:"role,omitempty"`
+	PreVerified  bool         `json:"pre_verified,string"`
+	PreConfirmed bool         `json:"pre_confirmed,string"`
+	PreApproved  bool         `json:"pre_approved,string"`
+}
+
+func (c *Client) AddListMember(sub *ListSubscriber) error {
+	b, err := json.Marshal(sub)
+	if err != nil {
+		return err
+	}
+
+	res, err := c.conn.do(http.MethodPost, buildURL("members"), bytes.NewBuffer(b))
+	if err != nil {
+		return err
+	}
+
+	if err := parseResponseError(res); err != nil {
+		return err
+	}
+
+	return res.Body.Close()
+}
+
+func (c *Client) GetListMembers(listID string) ([]*Member, error) {
+	params := map[string]string{
+		"list_id": listID,
+	}
+
+	b, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.conn.do(http.MethodGet, buildURL("members", "find"), bytes.NewBuffer(b))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := parseResponseError(res); err != nil {
+		return nil, err
+	}
+
+	pr := &PagedResult{}
+	if err = json.NewDecoder(res.Body).Decode(pr); err != nil {
+		return nil, err
+	}
+
+	members := make([]*Member, 0)
+	if err = json.Unmarshal(pr.Entries, &members); err != nil {
+		return nil, err
+	}
+
+	return members, res.Body.Close()
 }
--- member_test.go
+++ member_test.go
@@ -4,5 +4,43 @@ import (
 	"testing"
 )
 
+const (
+	memberDisplayName = "Jimmy Johns"
+	memberEmail       = "jimmyjohns@localhost.com"
+)
+
 func TestAddListMember(t *testing.T) {
+	c, err := NewClient(baseURL, username, password)
+	if err != nil {
+		t.Error(err)
+	}
+
+	sub := &ListSubscriber{
+		ListID:       listID,
+		Subscriber:   memberEmail,
+		DisplayName:  memberDisplayName,
+		PreVerified:  true,
+		PreConfirmed: true,
+		PreApproved:  true,
+	}
+
+	if err = c.AddListMember(sub); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGetListMembers(t *testing.T) {
+	c, err := NewClient(baseURL, username, password)
+	if err != nil {
+		t.Error(err)
+	}
+
+	members, err := c.GetListMembers(listID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(members) < 1 {
+		t.Errorf("Error list %s expected more than one member got none", listID)
+	}
 }
