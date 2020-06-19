ff406cdb7ac4ca2846bd1735453b9eac36131da5
Mac21 <russ.tolle@gmail.com>
2020-06-19 11:08:16 -0600

    Add member fields

--- member.go
+++ member.go
@@ -1,6 +1,13 @@
 package gomailman
 
 type Member struct {
+	ID               string           `json:"member_id"`
+	ListID           string           `json:"list_id"`
+	DisplayName      string           `json:"display_name"`
+	DeliveryMode     DeliveryMode     `json:"delivery_mode"`
+	SubscriptionMode SubscriptionMode `json:"subscription_mode"`
+	ModerationAction Action           `json:"moderation_action"`
+	Role             MemberRole       `json:"role"`
 }
 
 func (c *Client) AddListMember(member *Member) error {
